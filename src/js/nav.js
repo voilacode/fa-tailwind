ad5355aeb9ebff5865e15350115cf1531380bf8e
shifavoila <d4@voilacode.com>
2023-12-12 21:33:59 +0530

    updated index

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,7 +2,7 @@ const navLinks = document.querySelector('.nav-links');
 
       function onToggleMenu(icon){
         icon.name = icon.name === 'menu' ? 'close' : 'menu'
-        navLinks.classList.toggle('top-[10.5%]')
+        navLinks.classList.toggle('top-[10.8%]')
     }
 
 // modal
