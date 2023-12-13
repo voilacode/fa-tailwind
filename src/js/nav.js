7dd5e79632d3d28ac9c2558a11237aa13b45ad51
shifavoila <d4@voilacode.com>
2023-12-13 17:47:09 +0530

    changes

--- src/js/nav.js
+++ src/js/nav.js
@@ -17,10 +17,31 @@ function closeModal() {
 }
 
 //pop up
-function openPopUp() {
-    document.getElementById('popup').classList.remove('hidden');
-}
+const openModalButtons = document.querySelectorAll('[id^="openPopUp"]');
+    const closeModalButtons = document.querySelectorAll('[id^="closePopUp"]');
+    const modals = document.querySelectorAll('[id^="popup"]');
+    let scrollTop = 0;
 
-function closePopUp() {
-    document.getElementById('popup').classList.add('hidden');
-}
+    // Function to open the modal
+    const openPopUp = (modalIndex) => {
+        scrollTop = window.scrollY || document.documentElement.scrollTop;
+        modals[modalIndex].classList.remove('hidden');
+        document.body.style.overflow = 'hidden'; // Prevent scrolling on the background
+    };
+
+    // Function to close the modal
+    const closePopUp = (modalIndex) => {
+        modals[modalIndex].classList.add('hidden');
+        document.body.style.overflow = ''; // Allow scrolling on the background
+        window.scrollTo(0, scrollTop);
+    };
+
+    // Event listeners for opening modals
+    openModalButtons.forEach((button, index) => {
+        button.addEventListener('click', () => openPopUp(index + 1));
+    });
+
+    // Event listeners for closing modals
+    closeModalButtons.forEach((button, index) => {
+        button.addEventListener('click', () => closePopUp(index + 1));
+    });
