29c5642335895da24f14239805023916e706170a
shifavoila <d4@voilacode.com>
2023-12-14 14:25:45 +0530

    navbar updated

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,9 +1,61 @@
-const navLinks = document.querySelector('.nav-links');
+function toggleMobileMenu() {
+    const mainMenu = document.getElementById('mainMenu');
+    const menuIcon = document.querySelector('ion-icon[name="menu"]');
 
-      function onToggleMenu(icon){
-        icon.name = icon.name === 'menu' ? 'close' : 'menu'
-        navLinks.classList.toggle('top-[10.5%]')
+    mainMenu.classList.toggle('hidden');
+    menuIcon.name = mainMenu.classList.contains('hidden') ? 'menu' : 'close';
+}
+
+function closeOtherDropdowns(currentDropdownId) {
+    const dropdowns = document.querySelectorAll('.dropdown-menu');
+    dropdowns.forEach((dropdown) => {
+        if (dropdown.id !== currentDropdownId) {
+            dropdown.classList.add('hidden');
+        }
+    });
+}
+
+document.getElementById('dropdownToggle').addEventListener('click', function () {
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    closeOtherDropdowns('dropdownMenu');
+    dropdownMenu.classList.toggle('hidden');
+});
+
+document.getElementById('dropdownToggle1').addEventListener('click', function () {
+    var dropdownMenu1 = document.getElementById('dropdownMenu1');
+    closeOtherDropdowns('dropdownMenu1');
+    dropdownMenu1.classList.toggle('hidden');
+});
+
+document.getElementById('dropdownToggle2').addEventListener('click', function () {
+    var dropdownMenu2 = document.getElementById('dropdownMenu2');
+    closeOtherDropdowns('dropdownMenu2');
+    dropdownMenu2.classList.toggle('hidden');
+});
+
+// Close the dropdowns when clicking outside of them
+document.addEventListener('click', function (event) {
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    var dropdownToggle = document.getElementById('dropdownToggle');
+
+    if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.add('hidden');
     }
+
+    var dropdownMenu1 = document.getElementById('dropdownMenu1');
+    var dropdownToggle1 = document.getElementById('dropdownToggle1');
+
+    if (!dropdownToggle1.contains(event.target) && !dropdownMenu1.contains(event.target)) {
+        dropdownMenu1.classList.add('hidden');
+    }
+
+    var dropdownMenu2 = document.getElementById('dropdownMenu2');
+    var dropdownToggle2 = document.getElementById('dropdownToggle2');
+
+    if (!dropdownToggle2.contains(event.target) && !dropdownMenu2.contains(event.target)) {
+        dropdownMenu2.classList.add('hidden');
+    }
+});
 
 // modal
 function openModal() {
