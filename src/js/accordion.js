3c2ed9d5a0d8ca1d98828e10478f7467d5b89ffd
shaadomanthra <shaadomanthra@yahoo.com>
2023-12-15 19:19:26 +0530

    changes to js files and index

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,26 +1,28 @@
-var acc = document.getElementsByClassName("accordion");
-        var i;
 
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
+/** 
+ * Accordion script to handle multiple accordions 
+ * that have class name accordion 
+*/
+let acc = document.getElementsByClassName("accordion");
+let i;
 
-                var panel = this.nextElementSibling;
-
-                // Close all other panels
-                var allPanels = document.getElementsByClassName("panel");
-                for (var j = 0; j < allPanels.length; j++) {
-                    if (allPanels[j] !== panel) {
-                        allPanels[j].style.display = "none";
-                        // Remove "active" class from other buttons
-                        acc[j].classList.remove("active");
-                    }
-                }
-
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                } else {
-                    panel.style.display = "block";
-                }
-            });
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        // Close all other panels
+        let allPanels = document.getElementsByClassName("panel");
+        for (let j = 0; j < allPanels.length; j++) {
+            if (allPanels[j] !== panel) {
+                allPanels[j].style.display = "none";
+                // Remove "active" class from other buttons
+                acc[j].classList.remove("active");
+            }
         }
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
--- src/js/modals.js
+++ src/js/modals.js
@@ -1,37 +1,9 @@
 //pop up
 function openPopUp() {
-    document.getElementById('popup').style.display = 'flex';
-  }
+  document.getElementById('popup').style.display = 'flex';
+}
 
-  function closePopUp() {
-    document.getElementById('popup').style.display = 'none';
-  }
+function closePopUp() {
+  document.getElementById('popup').style.display = 'none';
+}
 
-// const openModalButtons = document.querySelectorAll('[id^="openPopUp"]');
-//     const closeModalButtons = document.querySelectorAll('[id^="closePopUp"]');
-//     const modals = document.querySelectorAll('[id^="popup"]');
-//     let scrollTop = 0;
-
-//     // Function to open the modal
-//     const openPopUp = (modalIndex) => {
-//         scrollTop = window.scrollY || document.documentElement.scrollTop;
-//         modals[modalIndex].classList.remove('hidden');
-//         document.body.style.overflow = 'hidden'; // Prevent scrolling on the background
-//     };
-
-//     // Function to close the modal
-//     const closePopUp = (modalIndex) => {
-//         modals[modalIndex].classList.add('hidden');
-//         document.body.style.overflow = ''; // Allow scrolling on the background
-//         window.scrollTo(0, scrollTop);
-//     };
-
-//     // Event listeners for opening modals
-//     openModalButtons.forEach((button, index) => {
-//         button.addEventListener('click', () => openPopUp(index + 1));
-//     });
-
-//     // Event listeners for closing modals
-//     closeModalButtons.forEach((button, index) => {
-//         button.addEventListener('click', () => closePopUp(index + 1));
-//     });
