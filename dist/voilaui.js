f5af3cee4d1dce797ab8fe2c321fb2ec57441baf
shifavoila <d4@voilacode.com>
2023-12-19 17:57:07 +0530

    changes in index

--- dist/voilaui.js
+++ dist/voilaui.js
@@ -100,6 +100,7 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         let panel = this.nextElementSibling;
+
         // Close all other panels
         let allPanels = document.getElementsByClassName("panel");
         for (let j = 0; j < allPanels.length; j++) {
@@ -107,12 +108,31 @@ for (i = 0; i < acc.length; i++) {
                 allPanels[j].style.display = "none";
                 // Remove "active" class from other buttons
                 acc[j].classList.remove("active");
+                // Reset the icon for other buttons
+                resetIcon(acc[j]);
             }
         }
+
         if (panel.style.display === "block") {
             panel.style.display = "none";
+            resetIcon(this);
         } else {
             panel.style.display = "block";
+            toggleIcon(this);
         }
     });
+}
+
+function resetIcon(element) {
+    let icon = element.querySelector('.icon');
+    if (icon) {
+        icon.innerHTML = '+';
+    }
+}
+
+function toggleIcon(element) {
+    let icon = element.querySelector('.icon');
+    if (icon) {
+        icon.innerHTML = '-';
+    }
 }
