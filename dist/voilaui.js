4385a31453031f3926aa0d991119bd1c06533d8c
SaiGaneshVoilacode <d7@voilacode.com>
2024-06-01 11:06:11 +0530

    Updated contact page

--- dist/voilaui.js
+++ dist/voilaui.js
@@ -214,35 +214,35 @@ function animateNumber(id, finalValue, duration) {
   
 
 // carousel
-const carouselItems = document.querySelectorAll('.carousel-item');
-let currentSlide = 0;
-const fadeDuration = 1000; 
-const slideInterval = 3000; 
+// const carouselItems = document.querySelectorAll('.carousel-item');
+// let currentSlide = 0;
+// const fadeDuration = 1000; 
+// const slideInterval = 3000; 
 
-function showSlide(slideIndex) {
-  carouselItems.forEach((item, index) => {
-    if (index === slideIndex) {
-      item.classList.remove('hidden', 'opacity-0');
-      item.classList.add('block', 'opacity-100');
-    } else {
-      item.classList.add('hidden', 'opacity-0');
-      item.classList.remove('block', 'opacity-100');
-    }
-  });
-}
+// function showSlide(slideIndex) {
+//   carouselItems.forEach((item, index) => {
+//     if (index === slideIndex) {
+//       item.classList.remove('hidden', 'opacity-0');
+//       item.classList.add('block', 'opacity-100');
+//     } else {
+//       item.classList.add('hidden', 'opacity-0');
+//       item.classList.remove('block', 'opacity-100');
+//     }
+//   });
+// }
 
-function nextSlide() {
-  carouselItems[currentSlide].classList.remove('opacity-100');
-  carouselItems[currentSlide].classList.add('opacity-0');
+// function nextSlide() {
+//   carouselItems[currentSlide].classList.remove('opacity-100');
+//   carouselItems[currentSlide].classList.add('opacity-0');
 
-  const nextSlideIndex = (currentSlide + 1) % carouselItems.length;
+//   const nextSlideIndex = (currentSlide + 1) % carouselItems.length;
 
-  setTimeout(() => {
-    currentSlide = nextSlideIndex;
-    showSlide(currentSlide);
-  }, fadeDuration / 2); 
-}
+//   setTimeout(() => {
+//     currentSlide = nextSlideIndex;
+//     showSlide(currentSlide);
+//   }, fadeDuration / 2); 
+// }
 
-showSlide(currentSlide);
+// showSlide(currentSlide);
 
-setInterval(nextSlide, slideInterval);
+// setInterval(nextSlide, slideInterval);
